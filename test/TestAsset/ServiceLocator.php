dcbc493bfcfca9c9c8fb186631baccaee6aa14ae
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:17:40 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/ServiceLocator.php
+++ test/TestAsset/ServiceLocator.php
@@ -1,16 +1,14 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Loader
+ * @see       https://github.com/laminas/laminas-loader for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-loader/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-loader/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Loader\TestAsset;
+namespace LaminasTest\Loader\TestAsset;
 
-use Zend\ServiceManager\ServiceLocatorInterface;
+use Laminas\ServiceManager\ServiceLocatorInterface;
 
 class ServiceLocator implements ServiceLocatorInterface
 {
