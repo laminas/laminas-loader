9cc063481cf94886f2618086baace5e898e34fbb
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:18:00 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/TestPluginMap.php
+++ test/TestAsset/TestPluginMap.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-loader for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-loader/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-loader/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Loader\TestAsset;
+namespace LaminasTest\Loader\TestAsset;
 
 /**
  * @group      Loader
@@ -21,8 +20,8 @@ class TestPluginMap implements \IteratorAggregate
      */
     public $map = array(
         'map'    => __CLASS__,
-        'test'   => 'ZendTest\Loader\PluginClassLoaderTest',
-        'loader' => 'Zend\Loader\PluginClassLoader',
+        'test'   => 'LaminasTest\Loader\PluginClassLoaderTest',
+        'loader' => 'Laminas\Loader\PluginClassLoader',
     );
 
     /**
