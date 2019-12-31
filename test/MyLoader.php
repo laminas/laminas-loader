ae2aec73c140e632a2a67bd3acc0e7c40aa7f8ae
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:17:45 -0600

    Rewriting as Laminas Project package

--- test/MyLoader.php
+++ test/MyLoader.php
@@ -1,21 +1,19 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Loader
+ * @see       https://github.com/laminas/laminas-loader for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-loader/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-loader/blob/master/LICENSE.md New BSD License
  */
 
 /**
  * Static methods for loading classes and files.
  *
- * @category   Zend
- * @package    Zend_Loader
+ * @category   Laminas
+ * @package    Laminas_Loader
  * @subpackage UnitTests
  */
-class Zend_Loader_MyLoader extends Zend_Loader
+class Laminas_Loader_MyLoader extends Laminas_Loader
 {
     public static function loadClass($class, $dirs = null)
     {
