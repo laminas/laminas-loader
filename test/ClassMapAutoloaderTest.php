c6815674d992a9518191a136e7515751abbf6e5a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:07:43 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- test/ClassMapAutoloaderTest.php
+++ test/ClassMapAutoloaderTest.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-loader for the canonical source repository
- * @copyright https://github.com/laminas/laminas-loader/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-loader/blob/master/LICENSE.md New BSD License
- */
-
 namespace LaminasTest\Loader;
 
 use Laminas\Loader\ClassMapAutoloader;
--- test/TestAsset/PrefixedClass.php
+++ test/TestAsset/PrefixedClass.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * @see       https://github.com/laminas/laminas-loader for the canonical source repository
- * @copyright https://github.com/laminas/laminas-loader/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-loader/blob/master/LICENSE.md New BSD License
- */
-
-/**
  * @group      Loader
  */
 class LaminasTest_UnusualPrefix_PrefixedClass
